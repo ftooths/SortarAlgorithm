5c4c26b32452b854e2d8283b6b7f4bf609010076
joungshc <joungshc@ajou.ac.kr>
2024-12-31 16:00:02 +0900

    add bubble Sort

--- codingTest/codingTest.cpp
+++ codingTest/codingTest.cpp
@@ -10,17 +10,31 @@ vector<int> initSetting(int size, int min, int max) {
 	vector<int> arr;
 
 	for (int i = 0; i < size; i++) {
-		arr.push_back(min + (rand()%(max-min+1)));
+		arr.push_back(min + (rand() % (max - min + 1)));
 	}
 	return arr;
 }
 
+vector<int> bubbleSort(vector<int> arr) {
+	int count = 0;
+	for (int i = 0; i < arr.size() - 1; i++) {
+		for (int j = 0; j < arr.size() - i - 1; j++) {
+			count++;
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+
+	cout << "연산 횟수: " << count << "\n";
+	return arr;
+} // 
 
 int main() {
-	vector<int>arr = initSetting(1000000, 1, 100);
+	vector<int>arr = initSetting(100000/2, 1, 1000);
+	vector<int>sortedArr = bubbleSort(arr);
 
-
-	for (const auto& num : arr) {
+	for (const auto& num : sortedArr) {
 		cout << num << "\n" << "";
 	}
 }
