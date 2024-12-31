fec7f19e01c3ecf82d5f02473bc29c62b9b43db0
joungshc <joungshc@ajou.ac.kr>
2024-12-31 15:03:59 +0900

    init setting

---
+++ codingTest/codingTest.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+vector<int> initSetting(int size, int min, int max) {
+	vector<int> arr;
+
+	for (int i = 0; i < size; i++) {
+		arr.push_back(min + (rand()%(max-min+1)));
+	}
+	return arr;
+}
+
+
+int main() {
+	vector<int>arr = initSetting(1000000, 1, 100);
+
+
+	for (const auto& num : arr) {
+		cout << num << "\n" << "";
+	}
+}
