bbb344c296c153f2e038fac668b9a255c3417e05
joungshc <joungshc@ajou.ac.kr>
2025-01-01 20:57:20 +0900

    insertionSort add

--- codingTest/codingTest.cpp
+++ codingTest/codingTest.cpp
@@ -45,12 +45,31 @@ vector<int> selectionSort(vector<int> arr) {
 	}
 	cout << "연산 횟수: " << count << "\n";
 	return arr;
+}// 선택 정렬
+
+vector<int> insertionSort(vector<int>arr) {
+	int count = 0;
+	for (int i = 1; i < arr.size() ; i++) {
+		int key = arr[i];
+		int j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			count++;
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		count++;
+		arr[j + 1] = key;
+	}
+	cout << "연산 횟수: " << count << "\n";
+	return arr;
 }
 
 int main() {
 	vector<int>arr = initSetting(1000, 1, 1000);
 	//vector<int>sortedArr = bubbleSort(arr);
-	vector<int>sortedArr = selectionSort(arr);
+	//vector<int>sortedArr = selectionSort(arr);
+	vector<int>sortedArr = insertionSort(arr);
 
 	for (const auto& num : sortedArr) {
 		cout << num << "\n" << "";
