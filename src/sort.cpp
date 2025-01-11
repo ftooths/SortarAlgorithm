3dae39ee414d07b43fbcafa1f8c503791c0b38a0
joungshc <joungshc@ajou.ac.kr>
2025-01-11 23:32:57 +0900

    버블, 선택, 삽입 정렬 넣었음. 내일 완성할 것.

--- src/sort.cpp
+++ src/sort.cpp
@@ -23,3 +23,50 @@ void Sort::printArray() const {
     }
     cout << endl;
 }
+
+void Sort::bubbleSort() {
+    count = 0; 
+    for (int i = 0; i < arr.size() - 1; i++) {
+        for (int j = 0; j < arr.size() - i - 1; j++) {
+            count++;
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    cout << "버블 정렬 연산 횟수: " << count << endl;
+}
+
+void Sort::selectionSort() {
+    count = 0; 
+    for (int i = 0; i < arr.size() - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < arr.size(); j++) {
+            count++;
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+        }
+    }
+    cout << "선택 정렬 연산 횟수: " << count << endl;
+}
+
+void Sort::insertionSort() {
+    count = 0; 
+    for (int i = 1; i < arr.size(); i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            count++;
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        count++;
+        arr[j + 1] = key;
+    }
+    cout << "삽입 정렬 연산 횟수: " << count << endl;
+}
